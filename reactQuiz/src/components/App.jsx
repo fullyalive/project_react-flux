266947f9c506277d0950a88deffd4f3e2fb88398
fullyalive <wogud1202@gmail.com>
2019-08-21 00:25:53 +0900

    quickquiz | Question & QuestionList Component

--- reactQuiz/src/components/App.jsx
+++ reactQuiz/src/components/App.jsx
@@ -1,9 +1,80 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import QuestionList from "./quiz/QuestionList.jsx";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [
+        {
+          id: 1,
+          text: "What is your name?",
+          choices: [
+            {
+              id: "a",
+              text: "fullyalive"
+            },
+            {
+              id: "b",
+              text: "jay"
+            },
+            {
+              id: "c",
+              text: "Mr.Lee"
+            }
+          ],
+          correct: "b"
+        },
+        {
+          id: 2,
+          text: "What is your elder cats name?",
+          choices: [
+            {
+              id: "a",
+              text: "kkung"
+            },
+            {
+              id: "b",
+              text: "jjong"
+            },
+            {
+              id: "c",
+              text: "ddung"
+            }
+          ],
+          correct: "a"
+        },
+        {
+          id: 3,
+          text: "What is your younger cats name?",
+          choices: [
+            {
+              id: "a",
+              text: "kkung"
+            },
+            {
+              id: "b",
+              text: "jjong"
+            },
+            {
+              id: "c",
+              text: "ddung"
+            }
+          ],
+          correct: "b"
+        }
+      ],
+      score: 0,
+      current: 1
+    };
+  }
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <QuestionList {...this.state} />
+      </div>
+    );
   }
 }
 
---
+++ reactQuiz/src/components/quiz/QuestionList.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Question from "./Question.jsx";
+
+class QuestionList extends Component {
+  render() {
+    return (
+      <div className="questions">
+        {this.props.questions.map(question => {
+          return (
+            <Question key={question.id} question={question} {...this.props} />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default QuestionList;
